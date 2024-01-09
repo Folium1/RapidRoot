00dc469b4dfde71cdd2186c3219e00d1fb25aa67
Folium <vampirowitch.vampir@gmail.com>
2024-01-09 21:31:40 +0200

    Refactoring

--- router.go
+++ router.go
@@ -1,229 +1,104 @@
-package rapidRoot
+package rapidroot
 
 import (
 	"fmt"
 	"net/http"
-	"strings"
 )
 
 type Router struct {
-	root       *node
-	routesList strings.Builder
+	tree       map[string]*node
+	routesList []byte
 }
 
 // NewRouter returns a new router instance with default configuration.
 func NewRouter() *Router {
-	r := &Router{
-		root: newNode(),
+	return &Router{
+		tree:       make(map[string]*node),
+		routesList: []byte("\n------------Handlers--------------\n\n"),
 	}
-	r.routesList.WriteString("\n------------Handlers--------------\n\n")
-	return r
 }
 
-type node struct {
-	pathSegment     string
-	method          string
-	handler         HandlerFunc
-	staticSegments  map[string]*node
-	dynamicSegments map[string]*node
-	middlewares     []Middleware
-}
-
-func newNode() *node {
-	return &node{
-		pathSegment:     "",
-		method:          "",
-		handler:         nil,
-		staticSegments:  make(map[string]*node),
-		dynamicSegments: make(map[string]*node),
-		middlewares:     make([]Middleware, 0),
+// Helper method to get or create the root node for a specific HTTP method.
+func (r *Router) getOrCreateRoot(method string) *node {
+	if root, ok := r.tree[method]; ok {
+		return root
 	}
+	root := newNode()
+	r.tree[method] = root
+	return root
 }
 
 func (r *Router) handle(method, path string, handler HandlerFunc) {
-	r.routesList.WriteString(fmt.Sprintf("%s %s %s\n", method, path, getFunctionName(handler)))
+	r.routesList = append(r.routesList, []byte(fmt.Sprintf("%s %s %s\n", method, path, getFunctionName(handler)))...)
 	if handler == nil {
-		log.fatal(fmt.Errorf("Nil handlers are not allowed | %s %s %s\n", method, path, "nil"))
+		log.fatal(fmt.Errorf("nil handlers are not allowed | %s %s %s\n", method, path, "nil"))
 	}
-	currentNode := r.root
-	pathSegments := strings.Split(path, "/")
 
-	for _, pathSegment := range pathSegments {
-		if currentNode.staticSegments == nil {
-			currentNode.staticSegments = make(map[string]*node)
-		}
+	path = cleanPath(path)
+	root := r.getOrCreateRoot(method)
 
-		if !strings.HasPrefix(pathSegment, "$") {
-			// handle static segments
-			if child, exists := currentNode.staticSegments[pathSegment]; exists {
-				currentNode = child
-			} else {
-				newNode := newNode()
-				currentNode.staticSegments[pathSegment] = newNode
-				currentNode = newNode
-			}
+	if path == "/" {
+		if root.pathSegment != "/" {
+			newRoot := newNode()
+			newRoot.pathSegment = "/"
+			newRoot.children = append(newRoot.children, root)
+			r.tree[method] = newRoot
 		} else {
-			// handle wildcard
-			dynamicSegment := pathSegment[1:]
-			if _, exists := currentNode.staticSegments["$"]; !exists {
-				newNode := newNode()
-				currentNode.staticSegments["$"] = newNode
-				currentNode = newNode
-				currentNode.pathSegment = dynamicSegment
-			} else {
-				currentNode = currentNode.staticSegments["$"]
-			}
+			root.handler = handler
 		}
+		return
 	}
 
-	currentNode.method = method
-	currentNode.handler = handler
+	node := getNode(path, root, nil)
+	if node == nil {
+		node = addRoute(path, root, handler)
+	}
+	node.handler = handler
 }
 
 func (r *Router) getHandler(method, path string, req *Request) HandlerFunc {
-	currentNode := r.root
-	pathSegments := strings.Split(path, "/")
-
-	for _, pathSegment := range pathSegments {
+	if root, ok := r.tree[method]; ok {
+		currentNode := getNode(path, root, req)
 		if currentNode == nil {
 			return nil
 		}
-		if child, isWild := currentNode.staticSegments["$"]; isWild {
-			currentNode = child
-			req.SetValue(currentNode.pathSegment, pathSegment)
-		} else if staticChild, ok := currentNode.staticSegments[pathSegment]; ok {
-			currentNode = staticChild
-		} else {
-			return nil
-		}
-	}
-
-	if currentNode.method == method {
 		return currentNode.handler
 	}
-
 	return nil
 }
 
-type Middleware func(HandlerFunc) HandlerFunc
-
-// MIDDLEWARE adds middleware to the route and all its children.
-// If path is "/" then the middleware will be applied to all routes
-// middlewares will execute in the order they are in the route
-// for example:
-//
-//	r.MIDDLEWARE("/", middleware1, middleware2)
-//	r.GET("/", handler)
-//	r.GET("/test", handler)
-//	r.Run(":8080")
-//
-// middleware1 and middleware2 will be applied to all routes. And middleware1 will be executed before middleware2.
-// If you want to apply middleware to a specific route, you can do it like this:
-//
-//	r.MIDDLEWARE("/test", middleware1, middleware2)
-//	r.GET("/", handler)
-//	r.GET("/test", handler)
-//	r.Run(":8080")
-func (r *Router) MIDDLEWARE(path string, middlewares ...Middleware) {
-	if path == "/" {
-		newRoot := newNode()
-		newRoot.staticSegments = r.root.staticSegments
-		newRoot.middlewares = middlewares
-		return
-	}
-
-	currentNode := r.root
-	pathSegments := strings.Split(path, "/")
-
-	for _, pathSegment := range pathSegments {
-		if currentNode.staticSegments == nil {
-			currentNode.staticSegments = make(map[string]*node)
-		}
-
-		if !strings.HasPrefix(pathSegment, "$") {
-			// handle static segments
-			if child, exists := currentNode.staticSegments[pathSegment]; exists {
-				currentNode = child
-			} else {
-				newNode := newNode()
-				currentNode.staticSegments[pathSegment] = newNode
-				currentNode = newNode
-			}
-		} else {
-			// handle wildcard
-			dynamicSegment := pathSegment[1:]
-			if _, exists := currentNode.staticSegments["$"]; !exists {
-				newNode := newNode()
-				currentNode.staticSegments["$"] = newNode
-				currentNode = newNode
-				currentNode.pathSegment = dynamicSegment
-			} else {
-				currentNode = currentNode.staticSegments["$"]
-			}
-		}
-	}
-	currentNode.middlewares = append(currentNode.middlewares, middlewares...)
-}
-
-// Helper method to apply middleware for the root node and all children.
-func (r *Router) applyMiddlewareForRoutes() {
-	r.traverseAndApplyMiddleware(r.root, r.root.middlewares...)
-}
-
-// Recursive method to traverse the route tree and apply middleware.
-func (r *Router) traverseAndApplyMiddleware(currentNode *node, middleWares ...Middleware) {
-	if currentNode == nil {
-		return
-	}
-
-	currentNode.middlewares = append(currentNode.middlewares, middleWares...)
-
-	// Apply middlewares to the current node
-	for _, middleware := range currentNode.middlewares {
-		currentNode.handler = middleware(currentNode.handler)
-	}
-
-	// Recursively traverse children with a copy of the middlewares
-	for _, child := range currentNode.staticSegments {
-		r.traverseAndApplyMiddleware(child, currentNode.middlewares...)
-	}
-	for _, child := range currentNode.dynamicSegments {
-		r.traverseAndApplyMiddleware(child, currentNode.middlewares...)
-	}
-}
-
 func (r *Router) GET(path string, handler HandlerFunc) {
-	r.handle(http.MethodGet, cleanPath(path), handler)
+	r.handle(http.MethodGet, path, handler)
 }
 
 func (r *Router) POST(path string, handler HandlerFunc) {
-	r.handle(http.MethodPost, cleanPath(path), handler)
+	r.handle(http.MethodPost, path, handler)
 }
 
 func (r *Router) DELETE(path string, handler HandlerFunc) {
-	r.handle(http.MethodDelete, cleanPath(path), handler)
+	r.handle(http.MethodDelete, path, handler)
 }
 
 func (r *Router) PATCH(path string, handler HandlerFunc) {
-	r.handle(http.MethodPatch, cleanPath(path), handler)
+	r.handle(http.MethodPatch, path, handler)
 }
 
 func (r *Router) PUT(path string, handler HandlerFunc) {
-	r.handle(http.MethodPut, cleanPath(path), handler)
+	r.handle(http.MethodPut, path, handler)
 }
 
 func (r *Router) OPTIONS(path string, handler HandlerFunc) {
-	r.handle(http.MethodOptions, cleanPath(path), handler)
+	r.handle(http.MethodOptions, path, handler)
 }
 
 func (r *Router) HEAD(path string, handler HandlerFunc) {
-	r.handle(http.MethodHead, cleanPath(path), handler)
+	r.handle(http.MethodHead, path, handler)
 }
 
 func (r *Router) CONNECT(path string, handler HandlerFunc) {
-	r.handle(http.MethodConnect, cleanPath(path), handler)
+	r.handle(http.MethodConnect, path, handler)
 }
 
 func (r *Router) TRACE(path string, handler HandlerFunc) {
-	r.handle(http.MethodTrace, cleanPath(path), handler)
+	r.handle(http.MethodTrace, path, handler)
 }
--- run.go
+++ run.go
@@ -1,4 +1,4 @@
-package rapidRoot
+package rapidroot
 
 import (
 	"fmt"
@@ -20,48 +20,67 @@ func (w *responseCodeWrapper) WriteHeader(statusCode int) {
 	w.ResponseWriter.WriteHeader(statusCode)
 }
 
+func (w *responseCodeWrapper) Write(data []byte) (int, error) {
+	if w.statusCode == 0 {
+		w.statusCode = http.StatusOK
+	}
+	return w.ResponseWriter.Write(data)
+}
+
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	responseWrapper := &responseCodeWrapper{w, 0}
-	reqStruct := newReq(responseWrapper, req)
+	resp := &responseCodeWrapper{w, 0}
+	reqStruct := getRequest(resp, req)
+	defer releaseRequest(reqStruct)
+
 	handler := r.getHandler(req.Method, cleanPath(req.URL.Path), reqStruct)
 	if handler == nil {
 		notFoundHandler(w, req)
 		return
 	}
 	reqStruct.handlerName = getFunctionName(handler)
+
 	done := make(chan struct{})
 	go func() {
-		handler(reqStruct)
+		handlerWrapper(handler, reqStruct)
 		done <- struct{}{}
 		close(done)
 	}()
 	<-done
 
-	log.logRequest(req.URL.Path, req.Method, responseWrapper.statusCode)
+	log.logRequest(req.URL.Path, req.Method, resp.statusCode)
+}
+
+func handlerWrapper(handler HandlerFunc, req *Request) {
+	handler(req)
+	if req.Writer.(*responseCodeWrapper).statusCode == 0 {
+		req.SetStatus(http.StatusOK)
+	}
 }
 
 // Run starts the HTTP server.
 func (r *Router) Run(addr string) {
-	if log.output == os.Stdout {
-		r.routesList.WriteString("\n----------------------------------\n\n")
-		logHandlers(r.routesList.String())
-		r.routesList.Reset()
-	}
+	r.addRoutesListSeparator()
 	r.applyMiddlewareForRoutes()
 	if err := http.ListenAndServe(addr, r); err != nil {
-		log.fatal(fmt.Errorf("%s : %w", "Couldn't start the server, err:", err))
+		log.fatal(fmt.Errorf("Couldn't start the server: %w", err))
 	}
 }
 
-// RunWithTLS starts the HTTPS server.
-func (r *Router) RunWithTLS(addr string, certFile string, keyFile string) {
-	if log.output == os.Stdout {
-		r.routesList.WriteString("\n----------------------------------\n\n")
-		logHandlers(r.routesList.String())
-		r.routesList.Reset()
+// addRoutesListSeparator adds a separator to the routes list for debugging purposes.
+func (r *Router) addRoutesListSeparator() {
+	if log.output != os.Stdout {
+		return
 	}
+	r.routesList = append(r.routesList, []byte("\n----------------------------------\n\n")...)
+	logHandlers(string(r.routesList))
+	r.routesList = nil
+}
+
+// RunWithTLS starts the HTTPS server.
+func (r *Router) RunWithTLS(addr, certFile, keyFile string) {
 	r.applyMiddlewareForRoutes()
-	if err := http.ListenAndServeTLS(addr, certFile, keyFile, r); err != nil {
-		log.fatal(fmt.Errorf("%s : %w", "Couldn't start the server, err:", err))
+	err := http.ListenAndServeTLS(addr, certFile, keyFile, r)
+	if err != nil {
+		log.fatal(fmt.Errorf("Couldn't start the server, err: %w", err))
 	}
 }
